7d9998c9e35b36cd4e82d2780679a8db82387563
AlexTheDevOnMicrobit <Alextout52@gmail.Com>
2025-01-14 17:32:51 +0100

    -Better Interface -Beta : Groupe switch for privacy

--- main.ts
+++ main.ts
@@ -1,4 +1,18 @@
+enum RadioMessage {
+    message1 = 49434,
+    switchradio = 52629
+}
 input.onButtonPressed(Button.A, function () {
+    Chat_1 += Chat_1 + 1
+})
+input.onButtonPressed(Button.AB, function () {
+    if (Chat_1 == -1) {
+        radio.sendMessage(RadioMessage.switchradio)
+        radio.setGroup(Chat_2 + 2)
+        basic.showString("GS")
+        basic.showNumber(Chat_2)
+        basic.clearScreen()
+    }
     if (Chat_1 == 0) {
         radio.sendString("Hello")
     }
@@ -17,16 +31,41 @@ input.onButtonPressed(Button.A, function () {
     if (Chat_1 == 5) {
         radio.sendString("More later ...")
     }
-})
-input.onButtonPressed(Button.AB, function () {
-    Chat_1 = 0
+    if (Chat_1 < -2) {
+        Chat_1 = -1
+        basic.showIcon(IconNames.No)
+        basic.pause(500)
+        basic.clearScreen()
+    }
 })
 radio.onReceivedString(function (receivedString) {
     basic.showString(receivedString)
+    basic.clearScreen()
 })
 input.onButtonPressed(Button.B, function () {
-    Chat_1 += Chat_1 + 1
+    Chat_1 += Chat_1 - 1
 })
+input.onLogoEvent(TouchButtonEvent.Pressed, function () {
+    basic.showArrow(ArrowNames.South)
+    radio.setTransmitPower(Power_Radio - 1)
+})
+radio.onReceivedMessage(RadioMessage.switchradio, function () {
+    radio.setGroup(Chat_2)
+    basic.showString("GS")
+    basic.showNumber(Chat_2)
+    basic.clearScreen()
+})
+let Chat_2 = 0
+let Power_Radio = 0
 let Chat_1 = 0
-radio.setGroup(1)
 Chat_1 = 0
+Power_Radio = 0
+radio.setTransmitPower(7)
+radio.setGroup(1)
+for (let index = 0; index < 3; index++) {
+    basic.showIcon(IconNames.Diamond)
+    basic.clearScreen()
+    basic.pause(150)
+}
+basic.showIcon(IconNames.Yes)
+basic.clearScreen()
