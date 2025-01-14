fa3691324efd19593b6ebfba8baff6afe0b58667
AlexTheDevOnMicrobit <Alextout52@gmail.Com>
2025-01-14 18:27:28 +0100

    -Real Boot System, -Chek Systeam, - Change groupe systeme, -Bug Fixes, - Change power of trasmition with the logo

--- main.ts
+++ main.ts
@@ -1,14 +1,26 @@
 enum RadioMessage {
     message1 = 49434,
-    switchradio = 52629
+    switchradio = 52629,
+    Recive = 33629
 }
+/**
+ */
+/**
+ * Besion : -Faire fonctionner le switch groupe, -Chek MEssage
+ * 
+ * boot système
+ */
 input.onButtonPressed(Button.A, function () {
     Chat_1 += Chat_1 + 1
+})
+radio.onReceivedMessage(RadioMessage.Recive, function () {
+    Receve = 1
 })
 input.onButtonPressed(Button.AB, function () {
     if (Chat_1 == -1) {
         radio.sendMessage(RadioMessage.switchradio)
-        radio.setGroup(Chat_2 + 2)
+        Chat_2 = Chat_2 + 1
+        radio.setGroup(Chat_2)
         basic.showString("GS")
         basic.showNumber(Chat_2)
         basic.clearScreen()
@@ -29,6 +41,9 @@ input.onButtonPressed(Button.AB, function () {
         radio.sendString("Good")
     }
     if (Chat_1 == 5) {
+        radio.sendString("What time it is ?")
+    }
+    if (Chat_1 == 6) {
         radio.sendString("More later ...")
     }
     if (Chat_1 < -2) {
@@ -37,8 +52,15 @@ input.onButtonPressed(Button.AB, function () {
         basic.pause(500)
         basic.clearScreen()
     }
+    if (Chat_1 > 6) {
+        Chat_1 = 6
+        basic.showIcon(IconNames.No)
+        basic.pause(500)
+        basic.clearScreen()
+    }
 })
 radio.onReceivedString(function (receivedString) {
+    radio.sendMessage(RadioMessage.Recive)
     basic.showString(receivedString)
     basic.clearScreen()
 })
@@ -46,10 +68,21 @@ input.onButtonPressed(Button.B, function () {
     Chat_1 += Chat_1 - 1
 })
 input.onLogoEvent(TouchButtonEvent.Pressed, function () {
+    Power_Radio = Power_Radio - 1
+    radio.setTransmitPower(Power_Radio)
     basic.showArrow(ArrowNames.South)
-    radio.setTransmitPower(Power_Radio - 1)
+    basic.pause(50)
+    basic.clearScreen()
+    basic.showNumber(Power_Radio)
+    basic.pause(50)
+    basic.clearScreen()
+    if (Power_Radio < 0) {
+        Power_Radio = 0
+        basic.showIcon(IconNames.No)
+    }
 })
 radio.onReceivedMessage(RadioMessage.switchradio, function () {
+    Chat_2 = Chat_2 + 1
     radio.setGroup(Chat_2)
     basic.showString("GS")
     basic.showNumber(Chat_2)
@@ -58,14 +91,27 @@ radio.onReceivedMessage(RadioMessage.switchradio, function () {
 let Chat_2 = 0
 let Power_Radio = 0
 let Chat_1 = 0
+let Receve = 0
+Receve = 0
+let Boot = 0
 Chat_1 = 0
-Power_Radio = 0
-radio.setTransmitPower(7)
+Power_Radio = 7
+radio.setTransmitPower(Power_Radio)
 radio.setGroup(1)
-for (let index = 0; index < 3; index++) {
+Boot = 1
+while (Boot == 0) {
+    basic.showIcon(IconNames.SmallDiamond)
     basic.showIcon(IconNames.Diamond)
     basic.clearScreen()
-    basic.pause(150)
+    basic.pause(75)
 }
 basic.showIcon(IconNames.Yes)
 basic.clearScreen()
+basic.forever(function () {
+    if (Receve == 1) {
+        Receve = 0
+        basic.showIcon(IconNames.Yes)
+        basic.pause(100)
+        basic.clearScreen()
+    }
+})
